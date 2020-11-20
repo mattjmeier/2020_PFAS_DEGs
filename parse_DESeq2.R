455759a7b50735ae0e37c24ada50481db61eb0da
mattjmeier <matthew.james.meier@gmail.com>
2020-11-20 16:08:01 -0500

    Update to make script portable with directory structure

--- parse_DESeq2.R
+++ parse_DESeq2.R
@@ -1,5 +1,7 @@
 library(tidyverse)
-data_dir <- "~/shared/data/2020_PFAS_files_for_anthony/" # set as required
+library(here)
+
+data_dir <- here() # set as required
 
 tsv_files <- fs::dir_ls(data_dir, regexp = "\\.txt$", recurse=1)
 test <- readr::read_tsv(tsv_files[1], col_names=T)
@@ -20,13 +22,13 @@ DEG_summary <- DESeq2Results %>% group_by(group) %>% dplyr::tally()
 genes_per_group <- DESeq2Results %>% group_by(group) %>% dplyr::select(geneSymbol)
 
 write.table(DEG_summary,
-            "~/shared/data/2020_PFAS_files_for_anthony/DEG_summary.txt",
+            paste0(here(),"/DEG_summary.txt"),
             sep="\t",
             quote=F,
             row.names=F)
 
 write.table(genes_per_group,
-            "~/shared/data/2020_PFAS_files_for_anthony/genes_per_group.txt",
+            paste0(here(),"/genes_per_group.txt"),
             sep="\t",
             quote=F,
             row.names=F)
